3db039cfb0ec045e484c777e516e1d0d6f074a72
Diogo Correia <me@diogotc.com>
2025-03-25 15:11:58 +0100

    chore: pin nixpkgs version
    This ensures reproducibility.

--- shell.nix
+++ shell.nix
@@ -1,13 +1,21 @@
-{pkgs ? import <nixpkgs> {}}: let
-  mypkgs = import ./nix {inherit pkgs;};
+let
+  # Pin specific version of nixpkgs to ensure reproducibility far into the future
+  # Pinned: nixos-24.11 2025-03-21
+  nixpkgs = builtins.fetchTarball {
+    url = "https://github.com/NixOS/nixpkgs/archive/7105ae3957700a9646cc4b766f5815b23ed0c682.tar.gz";
+    sha256 = "sha256-8XfURTDxOm6+33swQJu/hx6xw1Tznl8vJJN5HwVqckg=";
+  };
 in
-  pkgs.mkShell {
-    buildInputs = with pkgs; [
-      typst
-      typstyle
+  {pkgs ? import nixpkgs {}}: let
+    mypkgs = import ./nix {inherit pkgs;};
+  in
+    pkgs.mkShell {
+      buildInputs = with pkgs; [
+        typst
+        typstyle
 
-      (python3.withPackages (ps: [
-        mypkgs.pyre-check
-      ]))
-    ];
-  }
+        (python3.withPackages (ps: [
+          mypkgs.pyre-check
+        ]))
+      ];
+    }
