a17900a639725a6c76482de2e7190a9461fcb3ca
Diogo Correia <me@diogotc.com>
2025-04-09 17:21:16 +0200

    feat(tool): add pyre config structs

---
+++ tool/src/pyre/config.rs
@@ -0,0 +1,43 @@
+use serde::Serialize;
+
+#[derive(Debug, Serialize)]
+pub struct PyreConfiguration {
+    pub site_package_search_strategy: SitePackageSearchStrategy,
+    pub source_directories: Vec<String>,
+    pub taint_models_path: Vec<String>,
+}
+
+#[derive(Debug, Serialize)]
+pub enum SitePackageSearchStrategy {
+    None,
+    All,
+    Pep561,
+}
+
+#[derive(Debug, Serialize)]
+pub struct TaintConfig {
+    pub sources: Vec<TaintEntry>,
+    pub sinks: Vec<TaintEntry>,
+    pub features: Vec<TaintEntry>,
+    pub rules: Vec<TaintRule>,
+    pub options: TaintOptions,
+}
+
+#[derive(Debug, Serialize)]
+pub struct TaintEntry {
+    pub name: String,
+}
+
+#[derive(Debug, Serialize)]
+pub struct TaintRule {
+    pub name: String,
+    pub code: u32,
+    pub sources: Vec<String>,
+    pub sink: Vec<String>,
+}
+
+#[derive(Debug, Serialize)]
+pub struct TaintOptions {
+    pub maximum_overrides_to_analyze: u32,
+    pub maximum_trace_length: u32,
+}
