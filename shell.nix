fd95866dfcd5091f4f0b3b613347c84952e421a3
Diogo Correia <me@diogotc.com>
2025-05-06 17:42:08 +0200

    chore: upgrade nixpkgs rev

--- shell.nix
+++ shell.nix
@@ -1,9 +1,9 @@
 let
   # Pin specific version of nixpkgs to ensure reproducibility far into the future
-  # Pinned: nixos-unstable 2025-03-25
+  # Pinned: nixos-unstable 2025-05-04
   nixpkgs = builtins.fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/698214a32beb4f4c8e3942372c694f40848b360d.tar.gz";
-    sha256 = "sha256-hw63HnwnqU3ZQfsMclLhMvOezpM7RSB0dMAtD5/sOiw=";
+    url = "https://github.com/NixOS/nixpkgs/archive/979daf34c8cacebcd917d540070b52a3c2b9b16e.tar.gz";
+    sha256 = "sha256-uKCfuDs7ZM3QpCE/jnfubTg459CnKnJG/LwqEVEdEiw=";
   };
 in
   {pkgs ? import nixpkgs {}}: let
