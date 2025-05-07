38469b6811728ccdcb7d05ec6fe23ee8367d3f38
Diogo Correia <me@diogotc.com>
2025-05-07 13:10:08 +0200

    feat(tool): add flask pysa models

--- tool/src/pyre/models.rs
+++ tool/src/pyre/models.rs
@@ -6,6 +6,7 @@ use crate::python::PyLock;
 
 const DEFAULT_MODELS: &str = include_str!("../../taint-models/default.pysa");
 const DJANGO_MODELS: &str = include_str!("../../taint-models/django.pysa");
+const FLASK_MODELS: &str = include_str!("../../taint-models/flask.pysa");
 
 /// Calculate which taint models should be loaded based on the
 /// dependencies present.
@@ -17,6 +18,7 @@ pub fn write_taint_models(dest: &Path, lockfile: PyLock) -> Result<()> {
         #[allow(clippy::single_match)]
         match package.name.as_str() {
             "django" => writeln!(file, "{}", DJANGO_MODELS)?,
+            "flask" => writeln!(file, "{}", FLASK_MODELS)?,
             _ => {}
         }
     }
